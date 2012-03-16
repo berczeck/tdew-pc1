bfc39d6c176e453bd513791c7364f01b15250102
Luis Alexander Aldazabal Gil <berczeck_400@hotmail.com>
2012-03-15 21:52:39 -0500

    solucion pregunta 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-r = Role.create(description:'Admin')
-z = Role.create(description:'User')
-User.create(name:'Alex', firstname:'Aldazabal', lastname:'Gil', email:'alex@gmail.com', birthdate:'19860702',role:z)
-User.create(name:'Pamela', firstname:'Loarte', lastname:'Salas', email:'pame@gmail.com', birthdate:'19910310',role:z)
-User.create(name:'Edwin', firstname:'Lopez', lastname:'Gonzales', email:'edwin@gmai.com', birthdate:'19800625',role:r)
+users = User.create([
+	{name:'Alex', firstname:'Aldazabal', lastname:'Gil', email:'alex@gmail.com', birthdate:'19860702'},
+	{name:'Pamela', firstname:'Loarte', lastname:'Salas', email:'pame@gmail.com', birthdate:'19910310'},
+	{name:'Edwin', firstname:'Lopez', lastname:'Gonzales', email:'edwin@gmai.com', birthdate:'19800625'}
+])
+
+admins = User.create([
+	{name:'Alex', firstname:'Aldazabal', lastname:'Gil', email:'alex@gmail.com', birthdate:'19860702'}
+])
+
+Role.create(description:'Admin',users:admins)
+Role.create(description:'User',users:users)
